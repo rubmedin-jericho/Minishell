d4b23179b87ac88ad2929b64afad895535ef0de5
Ruben Medina Martinez <rubmedin@cbr14s4.42barcelona.com>
2025-09-29 19:59:52 +0200

    .h modificado, main arreglado, utils con el ft_strcmp

--- minishell_main.c
+++ minishell_main.c
@@ -6,7 +6,7 @@
 /*   By: rubmedin <rubmedin@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:25:43 by rubmedin          #+#    #+#             */
-/*   Updated: 2025/09/29 16:25:11 by rubmedin         ###   ########.fr       */
+/*   Updated: 2025/09/29 19:52:48 by rubmedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,40 @@
 int	main(void)
 {
 	char *str;
+	int contador;
 
 	printf(MINISHELL_BANNER);
+	contador = 0;
 	while(1)
 	{
-		str = readline("minishell> "); //La funcion readline(const char *str) es la que te lee el stdin(salida estandar del input) y te lo guarda en str.
-		if(str)
+		str = readline("minishell> ");
+		if (!str)
+			break;
+		if (str)
 			add_history(str);
+		if (!ft_strcmp(str, "exit"))
+			break;
+		if (contador == 100)
+		{
+			contador = 0;
+			rl_clear_history();
+		}
+		contador++;
 		printf("%s\n", str);
-		free(str);
 	}
+	rl_clear_history();
 	return (0);
 }
 
-/*	Uso readline porque la otra opcion seria read y usar read seria poner mas trabajo porque read tiene la limitacion de leer
+/*		readline()
+ *	-------------------
+ *	La funcion readline(const char *str) es la que te lee el stdin(salida estandar del input) y te lo guarda en str.
+ *	Uso readline porque la otra opcion seria read y usar read seria poner mas trabajo porque read tiene 
+ *	la limitacion de leer y readline tine ya incorporado como un historial de comandos y 
+ *	no necesita buffer size si no que pilla todo el comando.
  *
- *
+ *		add_history()
+ *	-------------------
+ *	El add_history sirve para el tema de agregar un mini historial de comandos.
  *
  * */
