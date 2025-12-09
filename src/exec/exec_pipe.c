825c12364ba8cd2752af855b63390fbd453a24c8
Juan Gustavo Marques Da Cunha <jmarques@cbr11s3.42barcelona.com>
2025-12-09 14:57:54 +0100

    add tests in main for execute and clean leaks

--- src/exec/exec_pipe.c
+++ src/exec/exec_pipe.c
@@ -11,9 +11,10 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+/*
 static void	child(t_pipe *p)
 {
+	printf("child\n\n");
 	close(p->pipe_fd[0]);
 	if (p->input_fd != STDIN_FILENO)
 	{
@@ -28,6 +29,7 @@ static void	child(t_pipe *p)
 
 static void	parent(t_pipe *p)
 {
+	printf("parent\n\n");
 	close(p->pipe_fd[1]);
 	if (p->input_fd != STDIN_FILENO)
 		close(p->input_fd);
@@ -61,36 +63,103 @@ static void	run_pipe(t_ast *node, t_shell *sh, int input_fd)
 	else
 		parent(&p);
 }
+*/
 
-void	execute_pipe_recursive(t_ast *node, t_shell *sh, int input_fd)
+int	exec_last_cmd(t_ast *node, t_shell *sh, int input_fd)
 {
 	pid_t	pid;
 
-	if (!node)
-		return ;
-	if (node->type != PIPE)
+	pid = fork();
+	if (pid < 0)
 	{
-		pid = fork();
-		if (pid == 0)
-		{
-			if (input_fd != STDIN_FILENO)
-			{
-				dup2(input_fd, STDIN_FILENO);
-				close(input_fd);
-			}
-			exec_node_no_fork(node, sh);
-			exit(EXIT_FAILURE);
-		}
+		perror("fork");
 		close(input_fd);
-		waitpid(pid, NULL, 0);
-		return ;
+		return (-1);
 	}
-	run_pipe(node, sh, input_fd);
+	if (pid == 0)
+	{
+		if (input_fd != STDIN_FILENO)
+		{
+			dup2(input_fd, STDIN_FILENO);
+			close(input_fd);
+		}
+		exec_node_no_fork(node, sh);
+		exit(sh->exit_status);
+	}
+	close(input_fd);
+	return (pid);
+}
+
+int	execute_pipe_recursive(t_ast *node, t_shell *sh, int input_fd)
+{
+	pid_t	pid;
+	int		pipe_fd[2];
+
+	if (node->type != PIPE)
+		return (exec_last_cmd(node, sh, input_fd));
+	if (pipe(pipe_fd) < 0)
+	{
+		perror("pipe");
+		close(input_fd);
+		return (-1);
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		close(input_fd);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		close(pipe_fd[0]);
+		if (input_fd != STDIN_FILENO)
+		{
+			dup2(input_fd, STDIN_FILENO);
+			close(input_fd);
+		}
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[1]);
+		exec_node_no_fork(node->left, sh);
+		exit(sh->exit_status);
+	}
+	close(pipe_fd[1]);
+	close(input_fd);
+	return (execute_pipe_recursive(node->right, sh, pipe_fd[0]));
+}
+
+void free_commands(char **cmds)
+{
+    int i = 0;
+    perror("cmd\n\n");
+    if (!cmds)
+        return ;
+    while (cmds[i])
+    {
+        free(cmds[i]);
+        cmds[i] = NULL;
+        i++;
+    }
+
+    free(cmds);
+    cmds = NULL;
 }
 
 void	execute_pipe(t_ast *node, t_shell *sh)
 {
+	int	status;
+	int	last_pid;
+
 	sh->in_pipeline = 1;
-	execute_pipe_recursive(node, sh, STDIN_FILENO);
+	last_pid = execute_pipe_recursive(node, sh, STDIN_FILENO);
 	sh->in_pipeline = 0;
+	waitpid(last_pid, &status, 0);
+	while (wait(NULL) > 0);
+	if (WIFEXITED(status))
+		sh->exit_status = WEXITSTATUS(status);
+	else
+		sh->exit_status = 1;
+	free_ast(sh->ast);
 }
--- src/exec/executor.c
+++ src/exec/executor.c
@@ -28,6 +28,24 @@ void	execute_simple(t_ast *node, t_shell *sh)
 	exit(1);
 }
 
+void free_ast(t_ast *node)
+{
+	int	i;
+
+	i = 0;
+	if (!node)
+		return ;
+	if (node->type == CMD && node->args)
+	{
+		while(node->args[i])
+			free(node->args[i++]);
+		free(node->args);
+	}
+	free_ast(node->left);
+	free_ast(node->right);
+	free(node);
+}
+
 void	exec_node_no_fork(t_ast *node, t_shell *sh)
 {
 	if (node->type == PIPE)
@@ -67,7 +85,7 @@ void	exec_node(t_ast *node, t_shell *sh)
 	if (pid == 0)
 	{
 		exec_node_no_fork(node, sh);
-		exit(sh->exit_status);
+		exit(0);
 	}
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
