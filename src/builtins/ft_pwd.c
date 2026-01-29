625c3dfd88d72586aa5c62f123595ae0106c5ba2
Juan Gustavo Marques Da Cunha <jmarques@car2s6.42barcelona.com>
2026-01-29 16:57:36 +0100

    add pwd errors

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -12,10 +12,26 @@
 
 #include "minishell.h"
 
+int	pwd_error(t_shell *sh)
+{
+	if (sh->ast->args[1] && sh->ast->args[1][0] == '-'
+		&& sh->ast->args[1][1])
+	{
+		printf("bash: pwd: %s: bad option\n", sh->ast->args[1]);
+		printf("pwd: usage: pwd\n");
+		sh->exit_status = 2;
+		return (-1);
+	}
+	return (0);
+}
+
 void	ft_pwd(t_shell *shell)
 {
 	char	*pwd;
 
+	shell->exit_status = 0;
+	if (pwd_error(shell) == -1)
+		return ;
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 	{
@@ -26,5 +42,4 @@ void	ft_pwd(t_shell *shell)
 	ft_putendl_fd(pwd, 1);
 	free(pwd);
 	pwd = NULL;
-	shell->exit_status = 0;
 }
